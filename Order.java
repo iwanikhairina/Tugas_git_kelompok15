0ee9cc7c31253c0f40dd1e646fc0f6ff9ed39e44
NoviaAngreini <131504682+NoviaAngreini@users.noreply.github.com>
2023-11-15 21:08:12 +0700

    Create Order.java

---
+++ Order.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Order {
+    private List<MenuItem> items;
+
+    public Order() {
+        this.items = new ArrayList<>();
+    }
+
+    public void addItem(MenuItem item) {
+        items.add(item);
+    }
+
+    public double calculateTotal() {
+        double total = 0;
+        for (MenuItem item : items) {
+            total += item.getPrice();
+        }
+        return total;
+    }
+}
